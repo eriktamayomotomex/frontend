edff97ce134b6ec3ca15ee7d686077ba55de0922
eriktamayomotomex <erik.tamayo@motomex.com>
2025-02-21 15:22:36 -0600

    Feat: Add Styles to NavBar

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom"
 export default function RegisterView() {
   return (
     <>
-        <nav>
-            <Link to="/auth/login">
-                ¿Ya tienes una cuenta? Inicia Sesion
-            </Link>
+        <h1 className="text-4xl text-white font-bold">Crear Cuenta</h1>
+        
+        <nav className="mt-10">
+            <Link 
+                className="text-center text-white text-lg block"
+                to="/auth/login"
+            >¿Ya tienes una cuenta? Inicia Sesion</Link>
         </nav>
     </>
   )
