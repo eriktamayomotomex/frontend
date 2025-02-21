af3d8b393024213cb2071217d969800459b49867
eriktamayomotomex <erik.tamayo@motomex.com>
2025-02-21 15:12:20 -0600

    Feat: Add Principal Layout

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom"
 export default function RegisterView() {
   return (
     <>
-        <div>RegisterView</div>
-
         <nav>
             <Link to="/auth/login">
                 ¿Ya tienes una cuenta? Inicia Sesion
